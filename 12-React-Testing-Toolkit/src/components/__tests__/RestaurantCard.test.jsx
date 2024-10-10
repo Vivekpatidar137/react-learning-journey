6e36bfc0b7f6854ca49c8c42923f848f43b69d05
Vivek Patidar <vivekpatidar775@gmail.com>
2024-10-10 21:50:18 +0530

    Added test for rendering the correct restaurant image with a mocked CDN URL
    Implemented a test to ensure the RestaurantCard component correctly renders the restaurant image using a mocked IMG_CDN_URL.
    The test verifies that the src attribute of the image matches the expected CDN URL combined with the cloudinaryImageId from the mock data (MOCK_DATA).

--- 12-React-Testing-Toolkit/src/components/__tests__/RestaurantCard.test.jsx
+++ 12-React-Testing-Toolkit/src/components/__tests__/RestaurantCard.test.jsx
@@ -2,9 +2,25 @@ import { render, screen } from "@testing-library/react";
 import RestaurantCard from "../RestaurantCard";
 import MOCK_DATA from "../__mocks__/resCardMock.json";
 import "@testing-library/jest-dom";
+// Mock the IMG_CDN_URL value
+jest.mock("../config", () => ({
+  IMG_CDN_URL:
+    "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/",
+}));
 
 test("Should render RestaurantCard component", () => {
   render(<RestaurantCard {...MOCK_DATA.info} />);
   const name = screen.getByText("Ratlami Teekha");
   expect(name).toBeInTheDocument();
+});
+
+test("Should render the correct restaurant image", () => {
+  render(<RestaurantCard {...MOCK_DATA.info} />);
+  const image = screen.getByRole("img");
+
+  // Compare with the mocked IMG_CDN_URL, not from MOCK_DATA
+  expect(image).toHaveAttribute(
+    "src",
+    `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${MOCK_DATA.info.cloudinaryImageId}`
+  );
 });
