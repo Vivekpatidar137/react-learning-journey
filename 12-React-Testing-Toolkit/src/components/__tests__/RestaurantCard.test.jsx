0b02addcd37102ed17ebee475fc85f34f7337405
Vivek Patidar <vivekpatidar775@gmail.com>
2024-10-10 22:28:58 +0530

    Added test to display average rating and cost for two in the RestaurantCard

--- 12-React-Testing-Toolkit/src/components/__tests__/RestaurantCard.test.jsx
+++ 12-React-Testing-Toolkit/src/components/__tests__/RestaurantCard.test.jsx
@@ -24,3 +24,9 @@ test("Should render the correct restaurant image", () => {
     `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${MOCK_DATA.info.cloudinaryImageId}`
   );
 });
+
+test("Should display average rating and cost for two", () => {
+  render(<RestaurantCard {...MOCK_DATA.info} />);
+  const ratingCostText = screen.getByText(`${MOCK_DATA.info.avgRatingString} star • ${MOCK_DATA.info.costForTwo}`);
+  expect(ratingCostText).toBeInTheDocument();
+});
