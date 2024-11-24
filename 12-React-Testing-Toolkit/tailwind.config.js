bcddf884a4e514ca36b8ede94579a769d75b646a
Vivek Patidar <vivekpatidar775@gmail.com>
2024-11-24 14:15:16 +0530

    Add custom no-scrollbar utility to Tailwind CSS configuration.

--- 12-React-Testing-Toolkit/tailwind.config.js
+++ 12-React-Testing-Toolkit/tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./src/**/*.{html,jsx}"],
+  content: ["./src/**/*.{html,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // Custom utilities can also be added here if needed
+    },
   },
-  plugins: [],
-}
-
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        ".no-scrollbar": {
+          "-ms-overflow-style": "none", // For IE 11
+          "scrollbar-width": "none", // For Firefox
+        },
+        ".no-scrollbar::-webkit-scrollbar": {
+          display: "none", // For Chrome, Safari, and Edge
+        },
+      });
+    },
+  ],
+};
